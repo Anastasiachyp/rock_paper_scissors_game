832829d0d7a5c072de47482ef91904af739a5eab
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-03-04 08:50:05 +0100

    add data cy and correct test to pass in cypress WIP still don't pass

--- cypress/integration/user_can_play_game.feature.js
+++ cypress/integration/user_can_play_game.feature.js
@@ -1,25 +1,40 @@
 describe("User can play game against the computer", () => {
-    beforeEach(() => {
-      cy.visit("http://localhost:3000");
-    });
-
-    it("User choose Paper", () => {
-        cy.get("button#paper").should("contain", "Paper")
-    });
-    
-    it("User choose Scissors", () => {
-        cy.get("button#scissors").should("contain", "Scissors")
-      });
-  
-    it("User choose Rock", () => {
-      cy.get("button#rock").should("contain", "Rock")
-    });
-  
-    it("User can see computers choice", () => {
-      cy.get("computer-play").should("contain", "Computer choise")
-    });
-  
-    it("User can see the results", () => {
-      cy.get("#results").should("contain", "You win!")
-    });
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
   });
+
+  it("User choose Paper", () => {
+    cy.get('[data-cy="Paper"]').click();
+    cy.get('[data-cy="user-selection"]').should(
+      "contain",
+      "Your choice: Paper"
+    );
+    cy.get('[data-cy="computer-selection"]').should("exist");
+    cy.get('[data-cy="result-message"]').should("exist");
+  });
+
+  it("User choose Scissors", () => {
+    cy.get('[data-cy="Scissors"]').click();
+    cy.get('[data-cy="user-selection"]').should(
+      "contain",
+      "Your choice: Scissors"
+    );
+    cy.get('[data-cy="computer-selection"]').should("exist");
+    cy.get('[data-cy="result-message"]').should("exist");
+  });
+
+  it("User choose Rock", () => {
+    cy.get('[data-cy="Rock"]').click();
+    cy.get('[data-cy="user-selection"]').should("contain", "Your choice: Rock");
+    cy.get('[data-cy="computer-selection"]').should("exist");
+    cy.get('[data-cy="result-message"]').should("exist");
+  });
+
+  it("User can see computers choice", () => {
+    cy.get("computer-play").should("contain", "Computer choise");
+  });
+
+  it("User can see the results", () => {
+    cy.get("#results").should("contain", "You win!");
+  });
+});
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,31 +2,38 @@ import React, { useState } from "react";
 import rock from "./sources/images/rock.png";
 import paper from "./sources/images/paper.png";
 import scissors from "./sources/images/scissors.png";
-import { Container } from "semantic-ui-react"
+import { Container } from "semantic-ui-react";
 
 const Button = (props) => {
   return (
     <div value={props.name} onClick={props.onClick}>
-      <img class="user-selection-img" src={props.img} alt="img" />
+      <img
+        class="user-selection-img"
+        data-cy={props.name}
+        src={props.img}
+        alt="img"
+      />
     </div>
   );
 };
 
-function App() {
+function GamePlay() {
   const [game, setGame] = useState({
-  userSelection: "",
-  computerSelection: "",
-  message: "",
-});
+    userSelection: "",
+    computerSelection: "",
+    message: "",
+  });
 
   const playGame = (event) => {
-    const user = event.target.parentNode.getAttribute("value")
-    const computer = ["Rock", "Paper", "Scissors"][Math.floor(Math.random() * 3)];
+    const user = event.target.parentNode.getAttribute("value");
+    const computer = ["Rock", "Paper", "Scissors"][
+      Math.floor(Math.random() * 3)
+    ];
 
     if (user === computer) {
       setGame({
-      ...game,
-      message: (game.message = "It's a tie!"),
+        ...game,
+        message: (game.message = "It's a tie!"),
       });
     } else if (
       (user === "Rock" && computer === "Scissors") ||
@@ -34,38 +41,67 @@ function App() {
       (user === "Scissors" && computer === "Paper")
     ) {
       setGame({
-      ...game,
-      message: (game.message = "You won!"),
+        ...game,
+        message: (game.message = "You won!"),
       });
     } else {
       setGame({
-      ...game,
-      message: (game.message = "You lost!"),
-    });
-  }
+        ...game,
+        message: (game.message = "You lost!"),
+      });
+    }
 
-  setGame({
-    ...game,
-    userSelection: user,
-    computerSelection: computer,
-   });
-};
+    setGame({
+      ...game,
+      userSelection: user,
+      computerSelection: computer,
+    });
+  };
 
   return (
-  <Container id="container">
-    <h1>Rock, paper and scissors Game</h1>
-      <div class='ui buttons'>
-      <Button name="Rock" onClick={ playGame } img={rock} />
-      <Button name="Paper" onClick={ playGame } img={paper} />
-      <Button name="Scissors" onClick={ playGame } img={scissors}/>
-    </div>
-    <h1>{game.userSelection === "" ? "Pick one!" : `Your choice: ${game.userSelection}`}</h1>
-     <img className="pc-selection-img" src={ game.computerSelection === "Rock" ? rock : game.computerSelection === "Paper" ? paper : scissors } alt="img"/>
-      <h1>Computer choise: {game.computerSelection}</h1>
-      <h1 className="message">{game.message}</h1>
+    <Container id="container">
+      <h1>Rock, paper and scissors Game</h1>
+      <div class="ui buttons">
+        <Button data-cy="rock" name="Rock" onClick={playGame} img={rock} />
+        <Button data-cy="paper" name="Paper" onClick={playGame} img={paper} />
+        <Button
+          data-cy="scissors"
+          name="Scissors"
+          onClick={playGame}
+          img={scissors}
+        />
+      </div>
+      <div data-cy="user-selection">
+        <h1>
+          {game.userSelection === ""
+            ? "Pick one!"
+            : `Your choice: ${game.userSelection}`}
+        </h1>
+      </div>
+      <h1>
+        {game.userSelection === ""
+          ? "Pick one!"
+          : `Your choice: ${game.userSelection}`}
+      </h1>
+      <img
+        className="pc-selection-img"
+        src={
+          game.computerSelection === "Rock"
+            ? rock
+            : game.computerSelection === "Paper"
+            ? paper
+            : scissors
+        }
+        alt="img"
+      />
+      <div data-cy="computer-selection">
+				<h1>Computer choise: {game.computerSelection}</h1>
+			</div>
+			<div data-cy="result-message">
+				<h1 className="message">{game.message}</h1>
+			</div>
     </Container>
   );
 }
 
-
-export default App;
+export default GamePlay;
