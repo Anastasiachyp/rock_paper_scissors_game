2e5c65be82daaef6538cd524c3d1392e394e6af8
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-03-04 08:13:34 +0100

    WIP add components Header, Footer, Game and Rules import index.css to index.js to have some style

--- cypress/integration/user_can_see_game_interface.feature.js
+++ cypress/integration/user_can_see_game_interface.feature.js
@@ -2,7 +2,7 @@ describe("User can see game interface", () => {
   it("successfully renders,", () => {
     cy.visit("http://localhost:3000");
     cy.contains("Rock, paper and scissors Game");
-    cy.contains('Pick one!');
-    cy.contains('Computer choise:');
+    cy.contains("Pick one!");
+    cy.contains("Computer choise:");
   });
 });
